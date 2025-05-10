1f34ba003fe08681d29df88c12d1086c9bbb2307
Belen <bcandelass01@educastillalamancha.es>
2025-05-10 14:31:38 +0200

    Documentación del método 'existe(...)' con javadoc

--- src/practicaGit/FUtil.java
+++ src/practicaGit/FUtil.java
@@ -4,6 +4,11 @@ import java.io.File;
 
 public class FUtil {
 
+	/**
+	 * Este método verifica si el fichero existe en el sistema
+	 * @param filename (nomnre del fichero)
+	 * @return true/false (dependiendo de si existe o no)
+	 */
 	public static boolean existe(String filename) {
 		if (filename == null || filename.trim().isEmpty()) {
             throw new IllegalArgumentException("El nombre del fichero no puede estar vacío o solo contener espacios.");
