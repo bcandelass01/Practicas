19389f7dad95d0c7a7fceb4a31e8e43d31dc254e
Belen <bcandelass01@educastillalamancha.es>
2025-05-10 14:08:25 +0200

    Añado el FUtil y la clase Main

---
+++ src/practicaGit/FUtil.java
@@ -0,0 +1,15 @@
+package practicaGit;
+
+import java.io.File;
+
+public class FUtil {
+
+	public static boolean existe(String filename) {
+		File file = new File(filename);
+		if (file.exists()) {
+		return true;
+		}
+		return false;
+		}
+
+}
---
+++ src/practicaGit/Main.java
@@ -0,0 +1,9 @@
+package practicaGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(FUtil.existe("test.txt"));
+		System.out.println(FUtil.existe(""));
+	}
+}
